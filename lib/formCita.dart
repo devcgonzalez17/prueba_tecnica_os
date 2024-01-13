e06e694f95d896642c226d4a17ce49c7bd2915da
unknown <devcgonzalez17@gmail.com>
2024-01-13 16:36:10 -0500

    form basic creado

--- lib/formCita.dart
+++ lib/formCita.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
-class CrearCita extends StatelessWidget {
+class CrearCita extends StatefulWidget {
+  const CrearCita({super.key});
+
+  @override
+  CrearCitaState createState() {
+    return CrearCitaState();
+  }
+}
+
+class CrearCitaState extends State<CrearCita> {
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController dateInput = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,13 +26,164 @@ class CrearCita extends StatelessWidget {
           padding: EdgeInsets.all(20.0),
           child: Container(
             child: Form(
+              key: _formKey,
               child: Column(
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Documento del Medico",
+                          hintText:
+                              "-Ingresa el numero de documento del medico",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ]),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Documento del paciente",
+                        hintText:
+                            "-Ingresa el numero de documento del paciente",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Flexible(
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              top: 10.0, bottom: 10.0, right: 10.0),
+                          child: TextFormField(
+                            controller: dateInput,
+                            validator: (val) {
+                              if (val == null || val.isEmpty) {
+                                return "Por favor selecciona la fecha de la cita";
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              labelText: "Fecha de la cita",
+                              hintText: "-Selecciona la fecha de la cita",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                            ),
+                            readOnly: true,
+                            onTap: () async {
+                              DateTime? pickedDate = await showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime(1950),
+                                  //DateTime.now() - not to allow to choose before today.
+                                  lastDate: DateTime(2100));
+
+                              if (pickedDate != null) {
+                                print(
+                                    pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
+                                String formattedDate =
+                                    DateFormat('yyyy-MM-dd').format(pickedDate);
+                                print(
+                                    formattedDate); //formatted date output using intl package =>  2021-03-16
+
+                                setState(() {
+                                  dateInput.text = formattedDate;
+                                });
+                              } else {}
+                            },
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              top: 10.0, bottom: 10.0, left: 10.0),
+                          child: TextFormField(
+                            validator: (val) {
+                              if (val == null || val.isEmpty) {
+                                return "Por favor selecciona la hora de la cita";
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              labelText: "Hora de la cita",
+                              hintText: "-Selecciona la hora de la cita",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Observaciones",
+                        hintText: "-Ingresa observaciones de la cita",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Estado de la cita",
+                        hintText: "-Ingresa el nombre del paciente",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    child: TextFormField(
+                      validator: (val) {
+                        if (val == null || val.isEmpty) {
+                          return "Por favor selecciona la fecha de la cita";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
                         labelText: "Nombre",
-                        hintText: "-ingresa el nombre del paciente"),
-                  )
+                        hintText: "-Ingresa el nombre del paciente",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data')),
+                        );
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
                 ],
               ),
             ),
